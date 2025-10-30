fd0e400c5ce8cb632f4eb623980b8e5b363a4806
Vishva Kumara <vishva8kumara@gmail.com>
2025-10-30 17:34:31 +0530

    chore: moved interface to top of code

--- repository.js
+++ repository.js
@@ -1,61 +1,6 @@
 
 const devices = {};
 
-function device(_id) {
-	const id = _id;
-	let windowStart = new Date();
-	let count = 0;
-	let sumTemp = 0;
-	let sumPres = 0;
-	let sumHumd = 0;
-	let sumWisp = 0;
-	let sumWdir = 0;
-	let sumRain = 0;
-	let recent;
-	//
-	this.enqueue = function(dat){
-		count += 1;
-		sumTemp += 1.0 * dat.temp;
-		sumPres += 1.0 * dat.prs;
-		sumHumd += 1.0 * dat.hum;
-		sumWisp += 1.0 * dat.wsp;
-		sumWdir += 1.0 * dat.wdir;
-		sumRain += 1.0 * dat.rin;
-		recent = dat;
-	};
-	//
-	this.getRecent = function(){
-		return recent;
-	};
-	//
-	this.aggregateOut = function(now){
-		if (count == 0) {
-			delete devices[ id ];
-			return false;
-		}
-		//
-		const avgTemp = sumTemp / count;
-		const avgPres = sumPres / count;
-		const avgHumd = sumHumd / count;
-		const avgWisp = sumWisp / count;
-		const avgWdir = sumWdir / count;
-		const totRain = sumRain;
-		const samples = count;
-		const windowSt = windowStart;
-		//
-		count = 0;
-		sumTemp = 0;
-		sumPres = 0;
-		sumHumd = 0;
-		sumWisp = 0;
-		sumWdir = 0;
-		sumRain = 0;
-		windowStart = now;
-		//
-		return [avgTemp, avgPres, avgHumd, avgWisp, avgWdir, totRain, samples, windowSt];
-	};
-};
-
 module.exports = {
 
 	listDevices: function(){
@@ -74,4 +19,65 @@ module.exports = {
 		}
 	}
 
+};
+
+function device(_id) {
+	const id = _id;
+	let windowStart = new Date();
+	let count = 0;
+	let sumTemp = 0;
+	let sumPres = 0;
+	let sumHumd = 0;
+	let sumWisp = 0;
+	let sumSinWdir = 0;
+	let sumCosWdir = 0;
+	let sumRain = 0;
+	let recent;
+	//
+	this.enqueue = function(dat){
+		count += 1;
+		sumTemp += 1.0 * dat.temp;
+		sumPres += 1.0 * dat.prs;
+		sumHumd += 1.0 * dat.hum;
+		sumWisp += 1.0 * dat.wsp;
+		sumSinWdir += Math.sin(dat.wdir * Math.PI / 180);
+		sumCosWdir += Math.cos(dat.wdir * Math.PI / 180);
+		sumRain += 1.0 * dat.rin;
+		recent = dat;
+	};
+	//
+	this.getRecent = function(){
+		return recent;
+	};
+	//
+	this.aggregateOut = function(now){
+		if (count == 0) {
+			delete devices[ id ];
+			return false;
+		}
+		//
+		const avgTemp = sumTemp / count;
+		const avgPres = sumPres / count;
+		const avgHumd = sumHumd / count;
+		const avgWisp = sumWisp / count;
+		let avgWdir = Math.atan2(sumSinWdir / count, sumCosWdir / count) * 180 / Math.PI;
+		const totRain = sumRain;
+		const samples = count;
+		const windowSt = windowStart;
+		//
+		if (avgWdir < 0)
+			avgWdir += 360;
+		//
+		count = 0;
+		sumTemp = 0;
+		sumPres = 0;
+		sumHumd = 0;
+		sumWisp = 0;
+		sumSinWdir = 0;
+		sumCosWdir = 0;
+		sumRain = 0;
+		windowStart = now;
+		//
+		return [avgTemp, avgPres, avgHumd, avgWisp, avgWdir, totRain, samples, windowSt];
+	};
 };
